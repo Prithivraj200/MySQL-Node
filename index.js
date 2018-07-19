25086830118d293726438d45188af64fcdc905e1
Prithivraj200 <prethive200@gmail.com>
2018-07-19 15:47:24 +0530

    v0.0.2

--- index.js
+++ index.js
@@ -3,21 +3,24 @@ const mysql=require('mysql');
 const app=require('express')();
 const bodyParser=require('body-parser');
 
-app.use(cors());
-app.use(bodyParser.json());
+app.use(cors()); // enable Cross-Origin Resource Sharing
+app.use(bodyParser.json()); // if you will be sending data in json format
 
-const port='8090';
+const port='8090'; // port number for server to run
 const connection=mysql.createConnection({
                  host:'localhost',
                  database:'mysql-db',
                  user:'mysql-username',
                  password:'mysql-password'
-               })
+               }) 
+// check https://www.npmjs.com/package/mysql
 
+//get method
 app.get('/',(req,res)=>{
     res.status(200).send('Restaurant server is up and running... !');
 })
 
+//post method
 app.post('/getUser',(req,res)=>{
     console.log(req.body);  //data passed as body
     connection.query(`select id from user`,(err,result,fields)=>{
@@ -26,6 +29,7 @@ app.post('/getUser',(req,res)=>{
     })
 })
 
+//to start a server
 app.listen(port,()=>{
     console.log(`Server is running in ${port}..`);
 })
